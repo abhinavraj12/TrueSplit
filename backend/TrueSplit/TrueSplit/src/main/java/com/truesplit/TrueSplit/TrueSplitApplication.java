5fb103be8ff8a60f0b1726d852bc7605b44787fe
Abhinav Singh <abhinavrajofficial12@gmail.com>
2025-10-28 22:01:57 +0530

    Refactored Auth module implementation

---
+++ backend/TrueSplit/TrueSplit/src/main/java/com/truesplit/TrueSplit/TrueSplitApplication.java
@@ -0,0 +1,14 @@
+package com.truesplit.TrueSplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueSplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueSplitApplication.class, args);
+        System.out.println("Backend Server Running");
+	}
+
+}
